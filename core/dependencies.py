5f0db6b493c364b5a53bd764c9b64b1bc243fb4f
benssutton <benssutton@hotmail.com>
2026-06-04 22:03:07 +0100

    fix: make rollback explicit in get_transaction_session

--- core/dependencies.py
+++ core/dependencies.py
@@ -21,8 +21,12 @@ def get_data_service():
 
 async def get_transaction_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
-        yield session
-        await session.commit()
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
 
 
 SettingDep = Annotated[Settings, Depends(get_settings)]
